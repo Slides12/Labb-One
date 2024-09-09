69d5231f4fb59a26c76803da16ecb02d40a2e374
Slides12 <djohan93@hotmail.com>
2024-09-09 16:58:48 +0200

    La till en variant 2 med Funktioner.

--- ConsoleLabb1/Program.cs
+++ ConsoleLabb1/Program.cs
@@ -1,4 +1,7 @@
-﻿// User interaction
+﻿// Variant 1  UTAN FUNKTIONER (Finns en variant 2 nedan med Funktioner)
+
+
+// User interaction 
 Console.WriteLine("Skriv en valfri sträng: ");
 string userInput = Console.ReadLine();
 Console.WriteLine();
@@ -12,6 +15,7 @@ long totalSum = 0;
 
 
 
+
 for (int i = 0; i < userInput.Length; i++)
 {
     for (int j = 0; j < userInput.Length; j++)
@@ -22,9 +26,9 @@ for (int i = 0; i < userInput.Length; i++)
             if (k >= i && k <= userInput.IndexOf(userInput[i], i + 1))          // k>= i börja på userInput[i] om k är mindre OCH index userInput[i] (bokstaven på denna plats) efter index i +1, dvs kolla inte med userInput[i] utan nästa bokstav i ledet.
             {
                 tempString += string.Join("", userInput[k]);                    // Bygg en temporär string med värdena som är mellan första och nästakommande värde
-                 
+
                 if (tempString.All(char.IsDigit))                               //Om alla värden i tempString är siffror, så är stringen giltlig.
-                {                            
+                {
                     isMoreThenOneCharacter = true;
 
                 }
@@ -72,3 +76,122 @@ for (int i = 0; i < userInput.Length; i++)
     Console.ForegroundColor = ConsoleColor.White;
 
 }
+
+
+
+
+
+
+
+// Variant 2  MED FUNKTIONER
+
+
+// User interaction
+Console.WriteLine("Skriv en valfri sträng: ");
+ string userInput2 = Console.ReadLine();
+Console.WriteLine();
+
+CheckAndColorString(userInput2);
+
+
+
+
+
+
+static void CheckAndColorString(string userInput)
+{
+    long totalSum = 0;
+
+    for (int i = 0; i < userInput.Length; i++)
+    {
+
+        totalSum += BuildASubstringAndReturnIfConditionMatch(userInput,i);
+
+    }
+
+
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine($"\nTotal: {totalSum}");
+}
+
+
+
+static long BuildASubstringAndReturnIfConditionMatch(string userInput, int index)
+{
+    string tempString = string.Empty;
+    if (ContainsMoreThenOneCharacter(userInput, index))
+    {
+        for (int i = 0;i < userInput.Length; i++) { 
+                
+            if(i >= index && i <= userInput.IndexOf(userInput[index], index + 1)) {  
+            tempString += string.Join("", userInput[i]);
+                
+
+            }
+
+        }
+        if (tempString.All(char.IsDigit) && tempString != "")
+        {
+            PrintAndColor(userInput, index);
+            long totalSum = long.Parse(tempString);
+            return totalSum;
+        }
+    }
+            return 0;
+
+}
+
+static bool ContainsMoreThenOneCharacter(string userInput, int startindex)
+{
+
+    for(int i = 1;i < userInput.Length;i++)
+    {
+        if (userInput[startindex] == userInput[i])
+        {
+            return true;
+        }
+    }
+
+
+    return false;
+}
+
+
+
+static int GetMaxIndexOfSecondChar(string userInput,int index)
+{
+    int maxIndex = userInput.IndexOf(userInput[index], index + 1);
+
+    return maxIndex;
+}
+
+
+static void PrintAndColor(string userInput, int index)
+{
+
+    
+
+
+    for (int i = 0; i < userInput.Length;i++)
+    {
+        if(i >= index && i < GetMaxIndexOfSecondChar(userInput, index) + 1)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(userInput[i]);
+
+        }
+        else 
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(userInput[i]);
+
+
+        }
+        }
+    Console.WriteLine();
+}
+
+
+
+
--- ConsoleLabb1/obj/Debug/net8.0/ConsoleLabb1.AssemblyInfo.cs
+++ ConsoleLabb1/obj/Debug/net8.0/ConsoleLabb1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleLabb1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c34611498a3154ff3511309abb796d735f85ff7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb5bbc962d864205e84a42a1bb9faa5c6f4d951")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleLabb1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleLabb1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
